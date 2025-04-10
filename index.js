3bc642044f2491459db839be70d171376166abb9
Ayamgorng <96842816+Ayamgorng@users.noreply.github.com>
2025-04-10 18:43:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,21 +4,24 @@ import QRCode from "qrcode"
 import fs from "fs"
 import {check} from "./log/index.js"
 
-// Default server port is 8080
-const port = 8080
+// Default server port adalah 8080
+//const port = 8080 //gunakan kalau mau hosting tapi bukan di heroku
+const port = process.env.PORT || 8080 // kalau mau pakai heroku
 
 const ServerInterface = new Server(port)
 const WhatsappInterface = new Whatsapp()
 WhatsappInterface.WAConnect()
 
+// Endpoint untuk cek status
 ServerInterface.server.get("/status", (req,res) => {
     let data = {
         status: "OK",
-        whatsapp: WhatsappInterface.status == 0 ? "Close" : WhatsappInterface.status == 1 ? "QR" : WhatsappInterface.status == 2 ? "Connecting" : "Open"
+        whatsapp: WhatsappInterface.status == 0 ? "Close" : WhatsappInterface.status == 1 ? "QR/Pairing" : WhatsappInterface.status == 2 ? "Connecting" : "Open"
     }
     res.send(JSON.stringify(data))
 })
 
+// Endpoint untuk menampilkan QR Code (jika tersedia)
 ServerInterface.server.get("/qr", async (req,res) => {
     let isQR = WhatsappInterface.qr ? true : false
     if(isQR) {
@@ -33,6 +36,17 @@ ServerInterface.server.get("/qr", async (req,res) => {
     }
 })
 
+// Endpoint untuk menampilkan Pairing Code (jika tersedia)
+ServerInterface.server.get("/pairing", async (req,res) => {
+    if(WhatsappInterface.pairing) {
+        res.send("<head> <meta http-equiv=\"refresh\" content=\"5\"> </head><p>Pairing Code: " + WhatsappInterface.pairing + "</p>")
+    }
+    else{
+        res.send("<script type='text/javascript'>document.location.href = '/qr'</script>")
+    }
+})
+
+// Endpoint utama, redirect ke QR jika belum connected
 ServerInterface.server.get("/", async (req, res) => {
     if(WhatsappInterface.status !== 3){
         res.redirect(302, "./qr")
@@ -40,20 +54,20 @@ ServerInterface.server.get("/", async (req, res) => {
     else{
         let html = fs.readFileSync("./public/index.html")
         html = Buffer.from(html).toString("utf-8")
-
         res.send(html)
     }
 })
 
+// Endpoint untuk mendapatkan count pesan yang terhapus
 ServerInterface.server.get("/count", async(req, res) => {
     let struct = {
         status: "OK",
         count: WhatsappInterface.count
     }
-
     res.send(JSON.stringify(struct))
 })
 
+// Endpoint untuk menampilkan log
 ServerInterface.server.get("/log", async(req, res) => {
     let logFile = "./cache_log/log.txt"
     let c = await check(logFile)
